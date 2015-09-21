edfdf706ca1c1d2fac062f2f548b7ee066686aa9
Tarun Kumar Sukhu <tksukhu@gmail.com>
2015-09-21 10:55:32 +0530

    Added Table support to sample 8

--- samples/scripts/sample8-radar.js
+++ samples/scripts/sample8-radar.js
@@ -90,6 +90,7 @@ Radar = (function (){
   function _drawBlips(svg, json_path) {
     d3.json(json_path, function (blipData) {
       _drawBlipsUpon(svg, blipData);
+      tabulate(blipData, ["name", "quadrant","description"]);
     });
   }
 
@@ -162,6 +163,7 @@ Radar = (function (){
         .on('mouseout', tip.hide);
     
     svg.call(tip);
+    
     return svg;
   }
 
@@ -204,11 +206,54 @@ Radar = (function (){
     return _point(xProjection(polarCoords.r, angleInRadians), yProjection(polarCoords.r, angleInRadians));
   }
 
+  
+//The table generation function
+  function tabulate(data, columns) {
+      var table = d3.select("#radar-table").append("table")
+              .attr("style", "margin-left: 150px")
+              .attr("class","table table-bordered table-striped table-condensed"),
+          thead = table.append("thead"),
+          tbody = table.append("tbody");
+
+      // append the header row
+      thead.append("tr")
+          .selectAll("th")
+          .data(columns)
+          .enter()
+          .append("th")
+              .text(function(column) { return column.capitalize(); });
+
+      // create a row for each object in the data
+      var rows = tbody.selectAll("tr")
+          .data(data)
+          .enter()
+          .append("tr");
+
+      // create a cell in each row for each column
+      var cells = rows.selectAll("td")
+          .data(function(row) {
+              return columns.map(function(column) {
+                  return {column: column, value: row[column]};
+              });
+          })
+          .enter()
+          .append("td")
+          .attr("style", "font-family: Josefin+Sans") // sets the font style
+              .html(function(d) { return d.value; });
+      
+      return table;
+  }
+  
+  String.prototype.capitalize = function() {
+	    return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+  
   return {
     draw: draw,
     reDraw: reDraw,
     zoomIn: zoomIn,
     zoomOut: zoomOut
   };
+  
 })();
 
